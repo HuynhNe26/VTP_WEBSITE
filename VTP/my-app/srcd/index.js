f9b18f9f8dd2215b18597dfab72ed7bef6f49dfe
HuynhNe26 <nguyenhoanghuynh.vn@gmail.com>
2025-06-16 22:16:59 +0700

    update

--- VTP/my-app/srcd/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Router from './router/router.js'; /* npm i react-router */
-import ReactDOM from 'react-dom/client'; /* npm install react-router-dom */
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
